67f64597e0a000094eaf65fa11aad135aebad4eb
STAN <dupdateske@gmail.com>
2026-03-13 18:16:10 +0300

    Fix allowed hosts for Render

--- DailyBuzz_updates/dailybuzz/settings.py
+++ DailyBuzz_updates/dailybuzz/settings.py
@@ -28,10 +28,14 @@ SECRET_KEY = 'django-insecure-qdn^$yiz7l!v0*(p@h%gzb*4m-qbuc#0vntr&@yhp6klc1rrku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com",
+]
 
 CSRF_TRUSTED_ORIGINS = [
-    origin for origin in os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",") if origin
+    "https://*.onrender.com",
 ]
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "change-me-in-production")

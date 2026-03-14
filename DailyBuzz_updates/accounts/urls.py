efa1e15c9bc16bf57592bdb32f9af3aa11169b28
STAN <dupdateske@gmail.com>
2026-03-14 14:41:00 +0300

    Fix password reset form and show validation errors

--- DailyBuzz_updates/accounts/urls.py
+++ DailyBuzz_updates/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import (
     home_view,
     signup_step1_view,
@@ -26,6 +27,38 @@ urlpatterns = [
 
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
+    path(
+    'password-reset/',
+    auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset_form.html',
+        email_template_name='accounts/password_reset_email.html',
+        subject_template_name='accounts/password_reset_subject.txt',
+        success_url='/accounts/password-reset/done/'
+    ),
+    name='password_reset'
+),
+path(
+    'password-reset/done/',
+    auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ),
+    name='password_reset_done'
+),
+path(
+    'reset/<uidb64>/<token>/',
+    auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html',
+        success_url='/accounts/reset/done/'
+    ),
+    name='password_reset_confirm'
+),
+path(
+    'reset/done/',
+    auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ),
+    name='password_reset_complete'
+),
 
     path('terms/', terms_view, name='terms'),
 
--- DailyBuzz_updates/dailybuzz/settings.py
+++ DailyBuzz_updates/dailybuzz/settings.py
@@ -147,6 +147,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "dupdateske@gmail.com")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "hejf tdvc xvfh zail")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "dupdateske@gmail.com")
